a0c0997284e9ef0fb4d965941fa072cee20daf83
Guorvo <lmcuchero@gmail.com>
2023-04-17 14:57:24 -0300

    added filter and related sorting fixes using memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import './App.css'
 import { type User } from './types'
 import { UsersList } from './components/UsersList'
@@ -8,6 +8,7 @@ function App () {
   const [showColors, setShowColors] = useState(false)
   const [sortByCountry, setSortByCountry] = useState(false)
   const originalUsers = useRef<User[]>([])
+  const [filterCountry, setFilterCountry] = useState<string | null>(null)
 
   const toggleColors = () => {
     setShowColors(!showColors)
@@ -38,11 +39,23 @@ function App () {
       })
   }, [])
 
-  const sortedUsers = sortByCountry
-    ? [...users].sort((a, b) => {
-        return a.location.country.localeCompare(b.location.country)
+  const filteredUsers = useMemo(() => {
+    console.log('filter')
+    return typeof filterCountry === 'string' && filterCountry.length > 0
+      ? users.filter(user => {
+        return user.location.country.toLowerCase().includes(filterCountry.toLowerCase())
       })
-    : users
+      : users
+  }, [users, filterCountry])
+
+  const sortedUsers = useMemo(() => {
+    console.log('sort')
+    return sortByCountry
+      ? [...filteredUsers].sort(
+          (a, b) => a.location.country.localeCompare(b.location.country)
+        )
+      : filteredUsers
+  }, [filteredUsers, sortByCountry])
 
   return (
     <div className="App">
@@ -57,6 +70,9 @@ function App () {
         <button onClick={handleReset}>
           Resetear estado
         </button>
+        <input placeholder='Filtra por pais' onChange={(e) => {
+          setFilterCountry(e.target.value)
+        }}/>
       </header>
       <UsersList deleteUser={handleDelete} showColors={showColors} users={sortedUsers} />
     </div>
