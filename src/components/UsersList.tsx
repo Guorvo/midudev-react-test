451018bd9eb60a8b9d979d6fc07bc66c9c343377
Guorvo <lmcuchero@gmail.com>
2023-04-17 13:04:20 -0300

    displaying data

---
+++ src/components/UsersList.tsx
@@ -0,0 +1,51 @@
+import { type User } from '../types'
+
+interface Props {
+  users: User[]
+}
+export function UsersList ({ users }: Props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Foto</th>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>País</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {
+          users.map(user => {
+            return (
+              <tr key={user.id.value}>
+                <td>
+                  <img src={user.picture.thumbnail}/>
+                </td>
+                <td>
+                  {user.name.first}
+                </td>
+                <td>
+                  {user.name.last}
+                </td>
+                <td>
+                  {user.location.country}
+                </td>
+                <td>
+                  <button>Borrar</button>
+                </td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </table>
+  )
+}
+
+// table, thead, tbody claves!
+// tr => row
+// th => celdas de header
+// td => cell
